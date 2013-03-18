0c89c3c48ea25663330c52e22894c1f66755a9ed
A.M. Knight <aknight@gmail.com>
2013-03-18 02:00:14 -0400

    first bits of the view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,13 +2,16 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
+import play.data.*;
+import static play.data.Form.*;
 
 import views.html.*;
+import models.*;
 
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render(Project.find.all(), Task.find.all()));
     }
-  
+
 }
