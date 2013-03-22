91d86586ae969607131ffcb276135d30312c653f
andrew.m.knight <andrew.m.knight@accenture.com>
2013-03-22 16:06:37 -0400

    adding annotation for security

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,7 +9,8 @@ import views.html.*;
 import models.*;
 
 public class Application extends Controller {
-  
+    
+    @Security.Authenticated(Secured.class)
     public static Result index() {
         return ok(index.render(Project.find.all(), Task.find.all()));
     }
